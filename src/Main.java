a653f46302c919e19a0cad9da3daaeb91eace7f5
Chetan <chetang1109@gmail.com>
2025-01-01 18:04:51 +0530

    Both client and server are working fine!

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import com.example.networking.Client;
+import com.example.networking.Server;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World");
+    }
+}
